745970fdad6ca0411564dacc63abea50c689d173
Edgar <edgano@gmail.com>
2017-03-17 13:51:32 +0100

    randomBucket

---
+++ randomBucket/build/CMakeFiles/randomBucket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/randomBucket.dir/src/randomBucket.cpp.o"
+  "randomBucket.pdb"
+  "randomBucket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/randomBucket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
